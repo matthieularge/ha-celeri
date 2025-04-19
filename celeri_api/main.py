ff0b65f4438c7a63944e77221a5711cb04a9a9c5
matthieularge <79526878+matthieularge@users.noreply.github.com>
2025-04-19 13:21:15 +0200

    Update main.py

--- celeri_api/main.py
+++ celeri_api/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import mysql.connector
-import os
 from datetime import date
 from fastapi.responses import JSONResponse
 import json

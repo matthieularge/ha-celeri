d50a7c6154a19a8c41db5004a17080dac4e16425
matthieularge <79526878+matthieularge@users.noreply.github.com>
2025-04-28 07:54:07 +0200

    Update main.py

--- celeri_api/main.py
+++ celeri_api/main.py
@@ -11,7 +11,7 @@ from enum import Enum
 from typing import Optional
 
 # TODO
-# sites web de laure => dans HA addon nginx
+# sites web de laure => dans HA addon nginx (voir conf Ovh) ou sur ex pi4
 # Let's encrypt (ou proxy ngix) sur port différent => gaffe accès externe / mariaDB + url depuis App HA
 # ngix RATA4pi@
 

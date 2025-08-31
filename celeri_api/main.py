898ba11af709a7465667f9e7c6711ba358111dd6
matthieularge <79526878+matthieularge@users.noreply.github.com>
2025-08-31 13:57:12 +0200

    Update main.py

--- celeri_api/main.py
+++ celeri_api/main.py
@@ -495,7 +495,7 @@ def init_dates(data: dict):
     try:
         start = datetime.strptime(data["start"], "%Y-%m-%d").date()
         end = datetime.strptime(data["end"], "%Y-%m-%d").date()
-        loue = bool(data.get("loue", True))
+        loue = bool(data.get("loue", False))
         weekend = bool(data.get("weekend", False))  
 
         logger.info(f"Airbnb init dates {loue} entre {start} et {end} (weekend {weekend})")

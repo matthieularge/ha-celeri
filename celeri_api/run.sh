18af77467ddbe1cb8b2110f4b0e68ae5a1bfa00e
matthieularge <79526878+matthieularge@users.noreply.github.com>
2026-01-04 01:19:22 +0100

    Fix gunicorn command in run.sh

--- celeri_api/run.sh
+++ celeri_api/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#exec gunicorn main:app --bind 0.0.0.0:8000 -w 2 -k uvicorn.workers.UvicornWorker
-exec gunicorn celeri_api.main:app --bind 0.0.0.0:8000 -w 2 -k uvicorn.workers.UvicornWorker
+exec gunicorn main:app --bind 0.0.0.0:8000 -w 2 -k uvicorn.workers.UvicornWorker
+#exec gunicorn celeri_api.main:app --bind 0.0.0.0:8000 -w 2 -k uvicorn.workers.UvicornWorker
